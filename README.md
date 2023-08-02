6406370cc017648374caf1d9c05b863d8b9f215b
Talyta Augusto <talitaaugustoaraujo@gmail.com>
2023-08-02 11:18:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Trabalho-Lista
+
+Com base no exercício feito em sala com prof Leo, com utilização de Array, onde criamos um sistema que armazena um nome no Array e permite ainda realizar as operações de edição e exclusão, você deve propor um problema que pode ser resolvido com base na mesma proposta de sistema.
---
+++ controller.js
@@ -0,0 +1,140 @@
+class Produto{
+
+constructor(){
+    this.id = 1;
+    this.arrayProdutos = [];
+    this.editId = null;
+}
+salvar(){
+    let produto = this.lerDados();
+
+    if(this.validaCampos(produto)){
+        if(this.editId == null){
+        this.adicionar(produto);
+        }else {
+            this.atualizar(this.editId, produto);
+        }
+    }
+
+    this.listaTabela();
+    this.cancelar();
+
+}
+listaTabela(){
+    let tbody = document.getElementById('tbody');
+    tbody.innerText = '';
+
+    for(let i = 0; i < this.arrayProdutos.length; i++ ) {
+        let tr = tbody.insertRow();
+
+        let td_id = tr.insertCell();
+        let td_produto = tr.insertCell();
+        let td_valor = tr.insertCell();
+        let td_acoes = tr.insertCell();
+
+        td_id.innerText = this.arrayProdutos[i].id;
+        td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+        td_valor.innerText = this.arrayProdutos[i].preco;
+
+        td_id.classList.add('center');
+
+        let imgEdit = document.createElement('img');
+        imgEdit.src = 'img/editar.jpg';
+        imgEdit.setAttribute("onclick","produto.preparaEditacao("+ JSON.stringify(this.arrayProdutos[i]) +")")
+
+        let imgDeletar = document.createElement('img');
+        imgDeletar.src = 'img/deletar.png';
+        imgDeletar.setAttribute("onclick","produto.deletar("+ this.arrayProdutos[i].id +")")
+
+        td_acoes.appendChild(imgEdit);
+        td_acoes.appendChild(imgDeletar);
+
+        
+    }
+}
+adicionar(produto){
+    produto.preco = parseFloat(produto.preco)
+    this.arrayProdutos.push(produto);
+    this.id++;
+}
+atualizar(id,produto){
+    for(let i = 0; i < this.arrayProdutos.length; i++){
+        if(this.arrayProdutos[i].id == id){
+            this.arrayProdutos[i].nomeProduto = produto.nomeProduto;
+            this.arrayProdutos[i].preco = produto.preco;
+        }
+    }
+}
+
+preparaEditacao(dados){
+    this.editId = dados.id;
+
+    document.getElementById('produto').value = dados.nomeProduto;
+    document.getElementById('preco').value = dados.preco;
+
+    document.getElementById('btn1').innerText = 'Atualizar';
+}
+
+lerDados(){
+    let produto = {}
+
+    produto.id = this.id;
+    produto.nomeProduto = document.getElementById('produto').value;
+    produto.preco = document.getElementById('preco').value;
+
+    return produto;
+
+}
+validaCampos(produto){
+    let msg = '';
+
+    if(produto.nomeProduto == ''){
+        msg += '- Informe o Nome do Produto \n';
+    }
+
+    if(produto.preco == ''){
+        msg += '- Informe o Preço do Produto \n';
+    }
+
+    if(msg != ''){
+        alert(msg)
+        return false
+    }
+
+    return true;
+}
+cancelar(){
+    document.getElementById('produto').value = '';
+    document.getElementById('preco').value = '';
+
+    document.getElementById('btn1').value = 'Salvar';
+    this.editId = null;
+
+}
+
+deletar(id){
+    if(confirm('Deseja realmente deletar o produto do ID ' + id)){
+        let tbody = document.getElementById('tbody');
+
+    for(let i = 0; i < this.arrayProdutos.length; i++){
+        if(this.arrayProdutos[i].id == id){
+            this.arrayProdutos.splice(i, 1);
+            tbody.deleteRow(i);
+        }
+    }
+            console.log(this.arrayProdutos);
+    }
+
+    }
+
+}
+
+var produto = new Produto();
+
+
+
+
+
+
+
+
